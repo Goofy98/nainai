e2ecdf3b8e770bb8e4a8156dcf32475d10615c59
Goofy98 <f950908@live.com>
2018-01-07 18:45:54 +0800

    mod js and css
    根据儿子显示二级菜单

--- js/mian-page.js
+++ js/mian-page.js
@@ -1,21 +1,41 @@
 // JavaScript Document
         var t;
-        $(".nav-li1").hover(function () {
-            clearTimeout(t);
-            t = setTimeout(function () {
-                $(".nav-li1").find(".card-nav").animate({
-                    'top': 40 + 'px',
-                    'opacity': 1
-                }, 200).addClass("dis");
-            }, 500);
-        });
+        // $(".nav-li1").hover(function () {
+        //     clearTimeout(t);
+        //     t = setTimeout(function () {
+        //         $(".nav-li1").find(".card-nav").animate({
+        //             'top': 40 + 'px',
+        //             'opacity': 1
+        //         }, 200).addClass("dis");
+        //     }, 500);
+        // });
     
-        $(".nav-li1").mouseleave(function () {
-            clearTimeout(t);
-            $(this).find(".card-nav").animate({
-                'top': 50 + 'px',
-                'opacity': 0
-            }, 200, function () {
-                $(".nav-li1").find(".card-nav").removeClass("dis");
-            });
+        // $(".nav-li1").mouseleave(function () {
+        //     clearTimeout(t);
+        //     $(this).find(".card-nav").animate({
+        //         'top': 50 + 'px',
+        //         'opacity': 0
+        //     }, 200, function () {
+        //         $(".nav-li1").find(".card-nav").removeClass("dis");
+        //     });
+        // });
+        $(function(){
+            $(".nav-ul li").hover(
+                function(){
+                    var is = $(this);
+                    t=setTimeout(function(){
+                        $(is).children("ul").animate({
+                            'top': 40 + 'px',
+                            'opacity': 1
+                        }, 200).addClass("dis");
+                    },1000);
+                },
+                function(){
+                    clearTimeout(t);
+                    $(this).children("ul").animate({
+                        'top': 50 + 'px',
+                        'opacity': 0
+                    }, 200).removeClass("dis");
+                }
+                );
         });
